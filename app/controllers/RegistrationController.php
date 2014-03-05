c95cc400d8cb370bd8caf346c10774ec8ea41221
Lalremruata <remruata.cse@gmail.com>
2014-03-05 17:09:48 +0530

    Registration

--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -5,4 +5,51 @@ class RegistrationController extends BaseController
 	{
 		return View::make('registration.index');
 	}
+
+	public function register()
+	{
+		$rules=array(
+			'username'		=>'required',
+			'password'		=>'required|alphaNum|min:5',
+			'full_name'		=>'required',
+			'address'		=>'required',
+			'designation'	=>'required',
+			'phone_no'		=>'required',
+			'email_id'		=>'required',
+			);
+
+		$validator = Validator::make(Input::all(), $rules);
+
+		if ($validator->fails()){
+			return Redirect::to('registration')
+			->withInput(Input::all())
+			->withErrors($validator);
+		}
+
+		else{
+
+			$user = Sentry::createUser(array(
+				'username' 		=> Input::get('username'),
+				'password' 		=> Input::get('password'),
+				
+				'address' 		=> Input::get('address'),
+				'email_id' 		=> Input::get('email_id'),
+				'phone_no' 		=> Input::get('phone_no'),
+				'designation' 	=> Input::get('designation'),
+				'activated'		=> 'false',
+				));	
+
+			
+
+			$adminGroup = Sentry::findGroupById(4);
+
+		    // Assign the group to the user
+		    $user->addGroup($adminGroup);
+		    return Redirect::to('login');
+
+
+
+			//$activationCode = $user->getActivationCode(); 
+		}
+	}
 }
--- app/views/registration/index.blade.php
+++ app/views/registration/index.blade.php
@@ -5,13 +5,59 @@
 	<h3><span class="fa fa-user"></span> Registration</h3>
 	<hr>
 
-	{{Form::open(array('url'=>'', 'method'=>'post', 'class'=>'form-horizontal'))}}
+{{ HTML::ul($errors->all()) }}
+	{{Form::open(array('url'=>'/registration', 'method'=>'post', 'class'=>'form-horizontal'))}}
+	<div class="form-group">
+		{{Form::label('full_name', 'Full Name', array('class'=>'col-md-4 control-label'))}}
+		<div class="col-md-8">
+			{{Form::text('full_name', '', array('class'=>'form-control'))}}
+		</div>
+	</div>
+	
 	<div class="form-group">
 		{{Form::label('username', 'Username', array('class'=>'col-md-4 control-label'))}}
 		<div class="col-md-8">
 			{{Form::text('username', '', array('class'=>'form-control'))}}
 		</div>
 	</div>
+	<div class="form-group">
+		{{Form::label('password', 'Password', array('class'=>'col-md-4 control-label'))}}
+		<div class="col-md-8">
+			{{Form::password('password', array('class'=>'form-control'))}}
+		</div>
+	</div>
+	<div class="form-group">
+		{{Form::label('password_confirmation', 'Confirm Password', array('class'=>'col-md-4 control-label'))}}
+		<div class="col-md-8">
+			{{Form::password('password_confirmation', array('class'=>'form-control'))}}
+		</div>
+	</div>	
+	<div class="form-group">
+		{{Form::label('address', 'Address', array('class'=>'col-md-4 control-label'))}}
+		<div class="col-md-8">
+			{{Form::textarea('address', '', array('class'=>'form-control'))}}
+		</div>
+	</div>
+	<div class="form-group">
+		{{Form::label('email_id', 'Email Id', array('class'=>'col-md-4 control-label'))}}
+		<div class="col-md-8">
+			{{Form::text('email_id', '', array('class'=>'form-control'))}}
+		</div>
+	</div>
+	<div class="form-group">
+		{{Form::label('phone_no', 'Phone No.', array('class'=>'col-md-4 control-label'))}}
+		<div class="col-md-8">
+			{{Form::text('phone_no', '', array('class'=>'form-control'))}}
+		</div>
+	</div>
+	<div class="form-group">
+		{{Form::label('designation', 'Designation', array('class'=>'col-md-4 control-label'))}}
+		<div class="col-md-8">
+			{{Form::text('designation', '', array('class'=>'form-control'))}}
+		</div>
+	</div>
+
+
 	<div class="form-group">
 		<div class="col-md-12 text-right">
 			<button class="btn btn-md btn-primary" type="submit" name="submit">Submit</button>
