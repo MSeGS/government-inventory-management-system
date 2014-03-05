3f635540e62371a1bcdc5bb957e3f77d8e255887
Alan Pachuau <alanpachuau@gmail.com>
2014-03-05 11:15:35 +0530

    Fix css and menu link

--- app/views/layout/partial/menu.blade.php
+++ app/views/layout/partial/menu.blade.php
@@ -1,10 +1,10 @@
 <div class="navbar-collapse collapse">
     <ul class="nav navbar-nav navbar-right">
         
-        @if(Auth::check())
-        <li {{Request::path() == 'sign-out'?'class="active"':''}}><a href="{{url('/sign-out')}}">Sign Out</a></li>
+        @if(Sentry::check())
+        <li {{Request::path() == 'logout'?'class="active"':''}}><a href="{{url('/logout')}}">Sign Out</a></li>
         @else
-        <li {{Request::path() == 'sign-in'?'class="active"':''}}><a href="{{url('/sign-in')}}">Sign In</a></li>
+        <li {{Request::path() == 'login'?'class="active"':''}}><a href="{{url('/login')}}">Sign In</a></li>
         <li {{Request::path() == 'registration'?'class="active"':''}}><a href="{{url('/registration')}}">Registration</a></li>
         <li {{Request::path() == 'help'?'class="active"':''}}><a href="{{url('/help')}}">Help</a></li>
         @endif
