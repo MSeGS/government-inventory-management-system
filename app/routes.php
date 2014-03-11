d937286a702b1814969ef70eb7336b5a5be6cec7
Lalremruata <remruata.cse@gmail.com>
2014-03-11 15:29:33 +0530

    routes filter updated

--- app/routes.php
+++ app/routes.php
@@ -14,11 +14,11 @@
 Route::get('/', array('before'=>'sentry', 'uses'=>'HomeController@index', 'as'=>'home.index'));
 
 Route::get('login', array('before'=>'sentry', 'uses'=>'AuthController@login', 'as'=>'login'));
-Route::post('login', array('before'=>'sentry', 'uses'=>'AuthController@authenticate'));
+Route::post('login', array('before'=>'sentry', 'uses'=>'AuthController@authenticate', 'as'=>'login.submit'));
 Route::get('logout', array('before'=>'sentry', 'uses'=>'AuthController@logout', 'as'=>'logout'));
 
 Route::get('registration', array('uses'=>'RegistrationController@index', 'as'=>'registration'));
-Route::post('registration', array('uses'=>'RegistrationController@register'));
+Route::post('registration', array('uses'=>'RegistrationController@register', 'as'=>'registration.submit'));
 
 Route::resource('user', 'UserController');
 Route::get('group/{id}/permission', array('uses'=>'GroupController@permission', 'as'=>'group.permission'));
