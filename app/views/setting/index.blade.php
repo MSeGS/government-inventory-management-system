16ffdafb038e07e32a2e4be32971e8e9e6abfa08
Zorempuia Chuauhang <zorema07@gmail.com>
2014-04-03 16:23:38 +0530

    Setting page for admin

--- app/views/setting/index.blade.php
+++ app/views/setting/index.blade.php
@@ -20,9 +20,10 @@
 				<td>{{Option::getTitle($setting->option_key)}}</td>
 				<td>{{$setting->option_data}}</td>
 				<td>
-					{{Form::open(array('url'=>'option/'.$setting->id, 'method'=>'delete'))}}
+					{{Form::open(array('url'=>'setting/'.$setting->id, 'method'=>'delete'))}}
 
-					<a href="{{route('option.edit', array($setting->id))}}" class="btn btn-xs btn-success tooltip-top" title="Edit option"><i class="fa fa-pencil"></i></a>
+					<a href="{{route('setting.edit', array($setting->id))}}" class="btn btn-xs btn-success tooltip-top" title="Edit setting"><i class="fa fa-pencil"></i></a>
+					<button type="submit" onclick="return confirm('Are you sure');" name="id" class="btn btn-xs btn-danger tooltip-top" title="Remove option" value="{{$setting->id}}"><i class="fa fa-times"></i></a>
 					{{Form::close()}}
 				</td>
 			</tr>
@@ -35,36 +36,38 @@
 
 <div class="col-md-4">
 	<div class="panel panel-default">
-		<div class="panel-heading"><h5 class="text-center"><?php echo _("NEW OPTION") ?></h5></div>
-			@if(Session::has('message'))
-				<div class="alert alert-success">
-					{{Session::get('message')}}	
-				</div>
-			@endif
-		{{Form::open(array('url'=>'setting', 'method'=>'post', 'class'=>'form-vertical'))}}
-		<div class="panel-body">	
-			<div class="form-group">
-				<?php echo Form::label('option_key', _('Option Key'), array('class'=>'control-label')) ?>
-				{{Form::select('option_key', $optionSelect, 'null',array('class' =>'dropdown input-sm form-control'))}}
-				@if($errors->has('option_key'))
-						<p class="help-block"><span class="text-danger">{{$errors->first('option_key')}}</span></p>
-						@endif
-			</div>
-
-			<div class="form-group">
-				<?php echo Form::label('option_data', _('Option Data'), array('class'=>'control-label'))?>
-				{{Form::textarea('option_data', '', array('class'=>'form-control input-sm','rows'=>'3'))}}
-				@if($errors->has('option_data'))
-						<p class="help-block"><span class="text-danger">{{$errors->first('option_data')}}</span></p>
-						@endif
-			</div>
-
+		<div class="panel-heading"><h5 class="text-center"><?php echo _("NEW SETTING") ?></h5></div>
 			
-			<div class="form-group text-right">
-				<button type="submit" name="submit" class="btn btn-primary btn-sm pull-right"><?php echo _('Submit') ?></button>
-			</div>
-			{{Form::close()}}
-		</div>
+				@if(Session::has('message'))
+					<div class="alert alert-success">
+						{{Session::get('message')}}	
+					</div>
+				@endif
+				{{Form::open(array('url'=>'setting', 'method'=>'post', 'class'=>'form-vertical'))}}
+				<div class="panel-body">	
+					<div class="form-group">
+						<?php echo Form::label('option_key', _('Setting Name'), array('class'=>'control-label')) ?>
+						{{Form::select('option_key', $optionSelect, 'null',array('class' =>'dropdown input-sm form-control'))}}
+						@if($errors->has('option_key'))
+								<p class="help-block"><span class="text-danger">{{$errors->first('option_key')}}</span></p>
+								@endif
+					</div>
+
+					<div class="form-group">
+						<?php echo Form::label('option_data', _('Setting Value'), array('class'=>'control-label'))?>
+						{{Form::textarea('option_data', '', array('class'=>'form-control input-sm','rows'=>'3'))}}
+						@if($errors->has('option_data'))
+								<p class="help-block"><span class="text-danger">{{$errors->first('option_data')}}</span></p>
+								@endif
+					</div>
+
+					
+					<div class="form-group text-right">
+						<button type="submit" name="submit" class="btn btn-primary btn-sm pull-right"><?php echo _('Submit') ?></button>
+					</div>
+					{{Form::close()}}
+				</div>
+		
 	</div>
 </div>
 @stop
