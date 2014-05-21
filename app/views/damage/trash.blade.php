802248bd7818be05c6fed15711af72a9fa542346
Alan Pachuau <alanpachuau@gmail.com>
2014-05-21 12:46:20 +0530

    cleanup git conflict on damage trash

--- app/views/damage/trash.blade.php
+++ app/views/damage/trash.blade.php
@@ -18,7 +18,7 @@
 		    		</div>
 				</div>
 			</div>
-<<<<<<< Updated upstream
+
 		{{Form::close()}}
 	</div>
 @stop
@@ -29,46 +29,6 @@
 		{{Session::get('delete')}}
 	</div>
 	@endif
-	
-	<div class="col-md-12">
-		<table class="table table-striped table-bordered">
-			<thead>
-				<tr>
-					<th class="col-md-1">#</th>
-					<th><?php echo _("Product Name") ?></th>
-					<th class="col-md-2"><?php echo _("Category") ?></th>
-					<th class="col-md-1"><?php echo _("Quantity") ?></th>
-					<th class="col-md-2"><?php echo _("Report Date") ?></th>
-					<th class="col-md-2"><?php echo _("Status") ?></th>
-					<th class="col-md-2"></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php $i=0; ?>
-				@foreach($damages as $damage)
-				<tr>
-					<td>{{++$i}}</td>
-					<td>{{$damage->product->name}}
-						<br>
-						<span class="text-muted"><small>{{$damage->note}}</small></span>
-					</td>
-					<td>{{$damage->product->category->category_name}}</td>
-					<td>{{$damage->quantity}}</td>
-					<td>{{date('dS F, Y h:iA', strtotime($damage->reported_at))}}</td>
-					<td>{{ucfirst($damage->status)}}</td>
-					<td>
-						{{Form::open(array('url'=>route('damage.delete', $damage->id), 'method'=>'delete'))}}
-						<button href="{{route('damage.restore', $damage->id)}}" class="btn btn-xs btn-success tooltip-top" title="<?php echo _('Restore Damage') ?>"><i class="fa fa-undo left"></i></button>
-						<button type="submit" onclick="return confirm <?php echo _('Are you sure') ?>;" name="id" class="btn btn-xs btn-danger tooltip-top" title="<?php echo _('Remove Damage Permanently') ?>" value="{{$damage->id}}"><i class="fa fa-times"></i></button>
-						{{Form::close()}}
-					</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-		{{$damages->links()}}
-=======
-		</div>
 
 		<div class="col-md-12">
 			<table class="table table-striped table-bordered">
@@ -111,7 +71,6 @@
 			{{$damages->links()}}
 
 		</div>
->>>>>>> Stashed changes
 	</div>
 </div>
 @stop
